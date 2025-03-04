76e9ec066195410c42119cab1b13e8ed770665a0
Aabid Dewan <aabid@Aabids-Air.lan>
2025-03-03 22:56:46 -0500

    code

---
+++ Human.py
@@ -0,0 +1,37 @@
+class Human:
+    def __init__(self, name, age, gender, health, speed, strength, durability, ability):
+        self.__name = name
+        self.__age = age
+        self.__gender = gender
+        self.__health = health
+        self.__speed = speed
+        self.__strength = strength
+        self.__durability = durability
+        self.__ability = ability
+
+    def name(self):
+        print("My name is", self.__name)
+
+    def age(self):
+        print("My age is", self.__age)
+
+    def gender(self):
+        if self.__gender == 1:
+            print("I am a male")
+        elif self.__gender == 2:
+            print("I am a female")
+        
+    def health(self):
+        print("Health:", self.__health)
+
+    def speed(self):
+        print("speed:", self.__speed)
+
+    def strength(self):
+        print("Strength:", self.__strength)
+
+    def durability(self):
+        print("Durability:", self.__durability)
+
+    def ability(self):
+        print("Ability:", self.__ability)
